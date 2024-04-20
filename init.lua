194d5f6d10c11b147ff673385ec2c366a3aba890
kevinakorner <kevinakorner@gmail.com>
2024-04-20 13:18:01 -0700

    added split definition call

--- init.lua
+++ init.lua
@@ -547,7 +547,10 @@ require("lazy").setup({
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					-- map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", function()
+						require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" })
+					end, "Jump to definition")
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
